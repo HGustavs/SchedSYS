af6423ac2626c96d2e5d7a44819259408e6d3717
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2020-02-13 21:39:59 +0100

    duuuh

--- showsched_demo.php
+++ showsched_demo.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
     <script src="canvas.js"></script>
-    <script src="showsched.js"></script>
+    <script src="showsched_demo.js"></script>
     
     <style>
         body {
