119e33b0228e1694d147455f1c6cc82d188c14c4
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2020-02-19 23:04:17 +0100

    Seed of recursive progression

--- showcourses_demo.js
+++ showcourses_demo.js
@@ -12,21 +12,21 @@ var colors = ["white","Gold","pink","yellow","CornflowerBlue"];
 var data=[
 	{prognamn:"WEBUG",x:0,y:0,years:3,year:[
 			{			// Year 1
-						"4":[{"name":"Datorgrafik","hp":"7.5","spd":0.5, "req":[],"code":"IT118G"},{"name":"Webbplatsdesign",hp:"7.5","spd":0.5, "req":[],"code":"IT108G"}],
-						"5":[{"name":"Grundläggande programmering med C++","hp":"7.5","spd":0.5, "req":[],"code":"IT120G"},{"name":"User Experience Design",hp:"7.5","spd":0.5, "req":[],"code":"IT111G"}],
-						"1":[{"name":"Databassystem","hp":"7.5","spd":0.5, "req":[],"code":"IT121G"},{"name":"Webbutveckling - mobilapplikationsdesign",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT108G","credits":7.5}]],"code":"IT331G"}],
-						"2":[{"name":"Webbutveckling - Programmering av mobila applikationer","hp":"7.5","spd":0.5, "req":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],"code":"IT351G"},{"name":"Webbutveckling - XML API",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT120G","credits":7.5}],[{"type":"course","code":"IT121G","credits":7.5}]],"code":"IT111G"}]		
+						"4":[{"name":"Datorgrafik","hp":"7.5","spd":0.5,"code":"IT118G"},{"name":"Webbplatsdesign",hp:"7.5","spd":0.5,"code":"IT108G"}],
+						"5":[{"name":"Grundläggande programmering med C++","hp":"7.5","spd":0.5,"code":"IT120G"},{"name":"User Experience Design",hp:"7.5","spd":0.5, "code":"IT111G"}],
+						"1":[{"name":"Databassystem","hp":"7.5","spd":0.5, "code":"IT121G"},{"name":"Webbutveckling - mobilapplikationsdesign",hp:"7.5","spd":0.5,"code":"IT331G"}],
+						"2":[{"name":"Webbutveckling - Programmering av mobila applikationer","hp":"7.5","spd":0.5,"code":"IT351G"},{"name":"Webbutveckling - XML API",hp:"7.5","spd":0.5,"code":"IT111G"}]		
 			},
 			{			// Year 2
-						"4":[{"name":"IT i organisationer - introduktion","hp":"7.5","spd":0.5, "req":[],"code":"IT110G"},{"name":"Databaskonstruktion",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT121G","credits":7.5}],[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],"code":"IT315G"}],
-						"5":[{"name":"Datakommunikation - Introduktion","hp":"7.5","spd":0.5, "req":[],"code":"IT119G"},{"name":"Webbprogrammering",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT331G","credits":7.5}],[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT108G","credits":7.5}]],"code":"IT311G"}],
-						"1":[{"name":"Objektorienterad programmering","hp":"7.5","spd":0.5, "req":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],"code":"IT366G"},{"name":"Software Engineering",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],"code":"IT386G"}],
-						"2":[{"name":"Projekt i software engineering","hp":"15.0","spd":1, "req":[[{"type":"course","code":"IT386G","credits":7.5}],[{"type":"course","code":"IT311G","credits":7.5},{"type":"course","code":"IT366G","credits":7.5}]],"code":"IT350G"}]		
+						"4":[{"name":"IT i organisationer - introduktion","hp":"7.5","spd":0.5,"code":"IT110G"},{"name":"Databaskonstruktion",hp:"7.5","spd":0.5, "code":"IT315G"}],
+						"5":[{"name":"Datakommunikation - Introduktion","hp":"7.5","spd":0.5,"code":"IT119G"},{"name":"Webbprogrammering",hp:"7.5","spd":0.5, "code":"IT311G"}],
+						"1":[{"name":"Objektorienterad programmering","hp":"7.5","spd":0.5,"code":"IT366G"},{"name":"Software Engineering",hp:"7.5","spd":0.5,"code":"IT386G"}],
+						"2":[{"name":"Projekt i software engineering","hp":"15.0","spd":1, "code":"IT350G"}]		
 			},
 			{			// Year 3
-						"4":[{"name":"Operativsystem","hp":"7.5","spd":0.5, "req":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],"code":"IT391G"},{"name":"Webbutveckling - content management och drift",hp:"7.5","spd":0.5, "req":[[{"type":"course","code":"IT350G","credits":15}]],"code":"IT509G"}],
-						"5":[{"name":"Webbteknologi - forskning och utveckling","hp":"7.5","spd":0.5, "req":[[{"type":"course","code":"IT350G","credits":15}],[{"type":"course","code":"IT315G","credits":7.5}]],"code":"IT515G"},{"name":"Informationssäkerhet - Introduktion",hp:"7.5","spd":0.5, "req":[],"code":"IT139G"}],
-						"1":[{"name":"Examensarbete i informationsteknologi med inriktning mot webbprogrammering",hp:"30",spd:1.0,"req":[[{"type":"course","code":"IT515G","credits":7.5}],[{"type":"course","code":"IT509G","credits":7.5}]],"code":"IT391G"}],
+						"4":[{"name":"Operativsystem","hp":"7.5","spd":0.5, "code":"IT391G"},{"name":"Webbutveckling - content management och drift",hp:"7.5","spd":0.5,"code":"IT509G"}],
+						"5":[{"name":"Webbteknologi - forskning och utveckling","hp":"7.5","spd":0.5,"code":"IT515G"},{"name":"Informationssäkerhet - Introduktion",hp:"7.5","spd":0.5,"code":"IT139G"}],
+						"1":[{"name":"Examensarbete i informationsteknologi med inriktning mot webbprogrammering",hp:"30",spd:1.0,"code":"IT391G"}],
 						"2":[]		
 			}
 	]},
@@ -72,6 +72,29 @@ var data=[
 	]
 	}
 ];
+
+var forrk={
+	"IT118G":[],
+	"IT108G":[],
+	"IT120G":[],
+	"IT111G":[],
+  "IT121G":[],
+	"IT331G":[[{"type":"course","code":"IT108G","credits":7.5}]],
+	"IT351G":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],
+	"IT111G":[[{"type":"course","code":"IT120G","credits":7.5}],[{"type":"course","code":"IT121G","credits":7.5}]],
+	"IT110G":[],
+	"IT315G":[[{"type":"course","code":"IT121G","credits":7.5}],[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],
+	"IT119G":[],
+	"IT311G":[[{"type":"course","code":"IT331G","credits":7.5}],[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT108G","credits":7.5}]],
+	"IT366G":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],
+	"IT386G":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],
+	"IT350G":[[{"type":"course","code":"IT386G","credits":7.5}],[{"type":"course","code":"IT311G","credits":7.5},{"type":"course","code":"IT366G","credits":7.5}]],
+	"IT391G":[[{"type":"course","code":"IT120G","credits":7.5},{"type":"course","code":"IT141G","credits":7.5}]],
+	"IT509G":[[{"type":"course","code":"IT350G","credits":15}]],
+	"IT515G":[[{"type":"course","code":"IT350G","credits":15}],[{"type":"course","code":"IT315G","credits":7.5}]],	
+	"IT139G":[],
+	"IT391G":[[{"type":"course","code":"IT515G","credits":7.5}],[{"type":"course","code":"IT509G","credits":7.5}]],
+};
 
 function mdown(event)
 {
@@ -214,7 +237,7 @@ function showdata() {
 												var course=period[l];
 												var coursew=(course.spd*coursewidth);
 												var courseh=(course.hp/15/course.spd);
-												str+="<div id="+course.code+" onclick='logReq(this,"+JSON.stringify(course.req)+");'	class='course' style='";
+												str+="<div id="+program.prognamn+course.code+" onclick='logReqe(event);'	class='course' style='";
 												str+="left:"+Math.round((coursepos*coursewidth*zoomfact)+(zoomfact*4))+"px;";
 												str+="top:"+Math.round((periodheight*k*zoomfact)+(zoomfact*4))+"px;";
 												str+="width:"+Math.round((coursew*zoomfact)-(zoomfact*10))+"px;";
@@ -263,7 +286,13 @@ function updatepos()
 		}
 }
 
-function logReq(selected_el,req){
+// Can now be made recursive ... id must contain both id of program and course since same course may appear in more than one program
+
+function logReq(program,course)
+{
+		var courseforrk=forrk[course];
+
+/*	
     //console.log(req);    
     let elements = Array.from(document.getElementsByClassName("selected-course"));
     for(let i=0;i<elements.length;i++){
@@ -276,6 +305,15 @@ function logReq(selected_el,req){
     let str = "Requires: ";    
     str += logReqRow(req);
     console.log(str);
+*/	
+}
+
+function logReqe(event){
+	
+		var program=event.target.id.substr(0,5);
+		var course=event.target.id.substr(5);
+	
+		logReq(program,course);
 }
 
 function logReqRow(row, color_idx=1){
