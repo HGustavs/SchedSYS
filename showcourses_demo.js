c296f108a3ac2eda5b90799a37c5cbfc93175c82
Henrik Gustavsson <HGustavs@users.noreply.github.com>
2020-02-24 01:48:27 +0100

    block duplicate arrows and simpler sorting

--- showcourses_demo.js
+++ showcourses_demo.js
@@ -287,55 +287,65 @@ function logReqe(event){
 		
     str = logReqRow(courseforrk,rprogram,rcourse,"and");	
 	
+		ctx.clearRect(0,0,2000,2000);
+		console.log(arrows.length);
 	
 		// Draw all arrows
 		for(var i=0;i<arrows.length;i++){
 				var arrow=arrows[i];
-				
+
+				// if(arrow.from.course=="IT311G"||arrow.to.course=="IT311G") console.log(arrow);
+				if(arrow.from.arr.issorted==false){
+						arrow.from.arr.issorted=true;
+						arrow.from.arr.sort(function(a, b){return Math.atan2(a.dy,a.dx)-Math.atan2(b.dy,b.dx)});
+				}
+				if(arrow.to.arr.issorted==false){			
+						arrow.to.arr.issorted=true;
+						arrow.to.arr.sort(function(a, b){return Math.atan2(a.dy,a.dx)-Math.atan2(b.dy,b.dx)});			
+				}
+			
 				// I believe that left/right should have x-major sorting instead of y major sorting
 				// Further testing needed to figure out if we have any situations with many crossing arrows?
-			
+				col="#000";
 				if(arrow.from.side=="top"){
-						arrow.from.arr.sort(function(a, b){if(a.dy==b.dy){return b.dx-a.dx}else{return b.dy-a.dy}});
 						var x1=arrow.from.box.left+((arrow.from.box.width/(arrow.from.arr.length+1))*findIndex(arrow.from.arr,arrow.id));
 						var y1=arrow.from.box.top;
 				}
 				if(arrow.from.side=="bottom"){
-						arrow.from.arr.sort(function(a, b){if(a.dy==b.dy){return a.dx-b.dx}else{return b.dy-a.dy}});
 						var x1=arrow.from.box.left+((arrow.from.box.width/(arrow.from.arr.length+1))*findIndex(arrow.from.arr,arrow.id));
 						var y1=arrow.from.box.bottom;
+						col="#08a";
 				}
 				if(arrow.from.side=="left"){
-						arrow.from.arr.sort(function(a, b){if(a.dx==b.dx){return a.dy-b.dy}else{return b.dx-a.dx}});
 						var x1=arrow.from.box.left;
 						var y1=arrow.from.box.top+((arrow.from.box.height/(arrow.from.arr.length+1))*findIndex(arrow.from.arr,arrow.id));
 				}
 				if(arrow.from.side=="right"){
-						arrow.from.arr.sort(function(a, b){if(a.dx==b.dx){return a.dy-b.dy}else{return b.dx-a.dx}});
 						var x1=arrow.from.box.right;
 						var y1=arrow.from.box.top+((arrow.from.box.height/(arrow.from.arr.length+1))*findIndex(arrow.from.arr,arrow.id));
+						col="#0a0";
 				}			
 				if(arrow.to.side=="top"){
-						arrow.to.arr.sort(function(a, b){if(a.dy==b.dy){return a.dx-b.dx}else{return b.dy-a.dy}});
+						arrow.to.arr.sort(function(a, b){return Math.atan2(b.dy,b.dx)-Math.atan2(a.dy,a.dx)});			
+						col="#884";
+						//arrow.to.arr.sort(function(a, b){if(a.dy==b.dy){return a.dx-b.dx}else{return b.dy-a.dy}});
 						var x2=arrow.to.box.left+((arrow.to.box.width/(arrow.to.arr.length+1))*findIndex(arrow.to.arr,arrow.id));
 						var y2=arrow.to.box.top;
 				}
 				if(arrow.to.side=="bottom"){
-						arrow.to.arr.sort(function(a, b){if(a.dy==b.dy){return a.dx-b.dx}else{return a.dy-b.dy}});
 						var x2=arrow.to.box.left+((arrow.to.box.width/(arrow.to.arr.length+1))*findIndex(arrow.to.arr,arrow.id));
 						var y2=arrow.to.box.bottom;
 				}
 				if(arrow.to.side=="left"){
 						var x2=arrow.to.box.left;
-						arrow.to.arr.sort(function(a, b){if(a.dx==b.dx){return b.dy-a.dy}else{return a.dx-b.dx}});
 						var y2=arrow.to.box.top+((arrow.to.box.height/(arrow.to.arr.length+1))*findIndex(arrow.to.arr,arrow.id));
 				}
 				if(arrow.to.side=="right"){
 						var x2=arrow.to.box.right;
-						arrow.to.arr.sort(function(a, b){if(a.dx==b.dx){return b.dy-a.dy}else{return a.dx-b.dx}});
 						var y2=arrow.to.box.top+((arrow.to.box.height/(arrow.to.arr.length+1))*findIndex(arrow.to.arr,arrow.id));
-				}			
-				drawArrow(x1,y1,x2,y2);
+
+				}
+				drawArrow(x1,y1,x2,y2,col);
 		}
 }
 
@@ -355,8 +365,8 @@ function logReqRow(row,program,course, mode, color_idx=1){
 						// For now we assume all second level arrays are "or" - this may need to be revised to support more "exotic" configs
 						str +=" ( "+logReqRow(r,program,course,"or",color_idx++)+" ) ";
         }else{
-						toreq=document.getElementById(program+course);
 						fromreq=startpoint=document.getElementById(program+r.code);
+						toreq=document.getElementById(program+course);
 						// Highlight requirement course
             if(fromreq!=null&&toreq!=null){
 								frbox=fromreq.getBoundingClientRect();
@@ -386,8 +396,11 @@ function logReqRow(row,program,course, mode, color_idx=1){
 								// This id lets us search for connectors
 								var currid=makeRandomID();
 							
-								fromobj={id:currid,dir:"from",dx:dx,dy:dy,box:frbox};
-								toobj={id:currid,dir:"to",dx:dx,dy:dy,box:tobox};
+								// Top left to top left dx,dy for sorting
+								fdx=tobox.left-frbox.left;
+								fdy=tobox.top-frbox.top;
+								fromobj={id:currid,dx:fdx,dy:fdy,course:r.code,box:frbox};
+								toobj={id:currid,dx:-fdx,dy:-fdy,course:course,box:tobox};
 
 								// Detect interconnection variant - overlap or else
 								if(dx==0){
@@ -429,12 +442,26 @@ function logReqRow(row,program,course, mode, color_idx=1){
 								if(toobj.side=="top")      toobj.arr=courses[program+course].top;
 								if(toobj.side=="bottom")   toobj.arr=courses[program+course].bottom;
 							
-								// Push data to use for sorting connection points to each end of arrow
-								fromobj.arr.push({dx:tobox.left,dy:tobox.top,id:currid});
-								toobj.arr.push({dx:frbox.left,dy:frbox.top,id:currid});
+								fromobj.arr.issorted=false;
+								toobj.arr.issorted=false;
 							
-								// Store current arrow
-								arrows.push({id:currid,from:fromobj,to:toobj});
+								// Store current arrow - we only push new arrows to stack
+								var found=false;
+								for(var ct=0;ct<arrows.length;ct++){
+										if(arrows[ct].from.course==r.code&&arrows[ct].to.course==course) found=true;
+								}
+								if(!found){
+										// Push data to use for sorting connection points to each end of arrow
+										fromobj.arr.push({dx:tobox.left,dy:tobox.top,id:currid});
+										toobj.arr.push({dx:frbox.left,dy:frbox.top,id:currid});
+								
+										arrows.push({id:currid,from:fromobj,to:toobj});
+								}else{
+//										fromobj.arr.push({dx:tobox.left,dy:tobox.top,id:currid});
+//										toobj.arr.push({dx:frbox.left,dy:frbox.top,id:currid});
+									
+//										arrows.push({id:currid,from:fromobj,to:toobj});								
+								}
 							
 								// Update styling
                 fromreq.classList.add("selected-course");                 
@@ -453,9 +480,12 @@ function logReqRow(row,program,course, mode, color_idx=1){
 // drawArrow - Canvas code for drawing a filled arrow
 //-------------------------------------------------------------------------------------------------
 
-function drawArrow(x1,y1,x2,y2)
+function drawArrow(x1,y1,x2,y2,col)
 {
 		ctx.lineWidth=2.0;
+	
+		ctx.strokeStyle=col;
+		ctx.fillStyle=col;
 	
 		// Reflect vector and make unit length * 3
 		dx=-(y2-y1);
